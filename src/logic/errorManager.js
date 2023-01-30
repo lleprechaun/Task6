de6b4b471f021b82985c6357fd918b6b9ad623b4
lleprechaun <lera.elganova00@mail.ru>
2023-01-31 01:10:20 +0400

    error message

---
+++ src/logic/errorManager.js
@@ -0,0 +1,33 @@
+
+class ErrorManager {
+
+    showError(error) {
+        const errorArea = document.createElement('div')
+        errorArea.innerHTML = `<label class="error-label">
+                                <input type="checkbox" class="errorCheckbox" autoComplete="off"/>
+                                    <div class="alert error">
+                                        <span class="errorText">
+                                        ${error}
+		                                <br class="clear"/>
+                                        </span>
+                                    </div>
+                                </label>`
+        document.body.appendChild(errorArea);
+
+        setTimeout(() => {
+            let animateBlock = errorArea.animate([
+                {opacity: 1},
+                {opacity: 0}
+            ], {duration: 400, easing: 'ease-in-out', fill: 'forwards'});
+            animateBlock.addEventListener('finish', () => { errorArea.classList.add('hiddenBlock') });
+        },4500)
+        setTimeout(() => {
+            document.body.removeChild(errorArea);
+        },5000)
+
+    }
+
+}
+
+const $error = new ErrorManager()
+export { $error };
